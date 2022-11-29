52c2306d73defb355bd09fe31eab7138f3a919a9
GGN_2015 <630230141@qq.com>
2022-11-30 07:39:29 +0800

    +CG1 绘制正确颜色

--- CG10612/CG10612/CG10612View.cpp
+++ CG10612/CG10612/CG10612View.cpp
@@ -2,8 +2,8 @@
 // CG10612View.cpp: CCG10612View 类的实现
 //
 
-#include "pch.h"
 #include "framework.h"
+#include "pch.h"
 // SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
 // ATL 项目中进行定义，并允许与该项目共享文档代码。
 #ifndef SHARED_HANDLERS
@@ -17,89 +17,305 @@
 #define new DEBUG_NEW
 #endif
 
-
 // CCG10612View
 
 IMPLEMENT_DYNCREATE(CCG10612View, CView)
 
 BEGIN_MESSAGE_MAP(CCG10612View, CView)
-	// 标准打印命令
-	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
-	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
-	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+// 标准打印命令
+ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+ON_WM_LBUTTONDOWN()
+ON_WM_LBUTTONUP()
+ON_WM_MOUSEMOVE()
+ON_COMMAND(ID_SETNODE, &CCG10612View::OnSetnode)
+ON_COMMAND(ID_DELETENODE, &CCG10612View::OnDeletenode)
 END_MESSAGE_MAP()
 
 // CCG10612View 构造/析构
 
-CCG10612View::CCG10612View() noexcept
-{
-	// TODO: 在此处添加构造代码
-
+CCG10612View::CCG10612View() noexcept {
+  // TODO: 在此处添加构造代码
 }
 
-CCG10612View::~CCG10612View()
-{
-}
+CCG10612View::~CCG10612View() {}
 
-BOOL CCG10612View::PreCreateWindow(CREATESTRUCT& cs)
-{
-	// TODO: 在此处通过修改
-	//  CREATESTRUCT cs 来修改窗口类或样式
+BOOL CCG10612View::PreCreateWindow(CREATESTRUCT& cs) {
+  // TODO: 在此处通过修改
+  //  CREATESTRUCT cs 来修改窗口类或样式
 
-	return CView::PreCreateWindow(cs);
+  return CView::PreCreateWindow(cs);
 }
 
 // CCG10612View 绘图
 
-void CCG10612View::OnDraw(CDC* /*pDC*/)
-{
-	CCG10612Doc* pDoc = GetDocument();
-	ASSERT_VALID(pDoc);
-	if (!pDoc)
-		return;
+void CCG10612View::OnDraw(CDC* /*pDC*/) {
+  CCG10612Doc* pDoc = GetDocument();
+  ASSERT_VALID(pDoc);
+  if (!pDoc) return;
 
-	// TODO: 在此处为本机数据添加绘制代码
+  // TODO: 在此处为本机数据添加绘制代码
+  MyFunc_ShowAllItem();
 }
-
 
 // CCG10612View 打印
 
-BOOL CCG10612View::OnPreparePrinting(CPrintInfo* pInfo)
-{
-	// 默认准备
-	return DoPreparePrinting(pInfo);
+BOOL CCG10612View::OnPreparePrinting(CPrintInfo* pInfo) {
+  // 默认准备
+  return DoPreparePrinting(pInfo);
 }
 
-void CCG10612View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
-{
-	// TODO: 添加额外的打印前进行的初始化过程
+void CCG10612View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/) {
+  // TODO: 添加额外的打印前进行的初始化过程
 }
 
-void CCG10612View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
-{
-	// TODO: 添加打印后进行的清理过程
+void CCG10612View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/) {
+  // TODO: 添加打印后进行的清理过程
 }
-
 
 // CCG10612View 诊断
 
 #ifdef _DEBUG
-void CCG10612View::AssertValid() const
-{
-	CView::AssertValid();
-}
+void CCG10612View::AssertValid() const { CView::AssertValid(); }
 
-void CCG10612View::Dump(CDumpContext& dc) const
-{
-	CView::Dump(dc);
-}
+void CCG10612View::Dump(CDumpContext& dc) const { CView::Dump(dc); }
 
-CCG10612Doc* CCG10612View::GetDocument() const // 非调试版本是内联的
+CCG10612Doc* CCG10612View::GetDocument() const  // 非调试版本是内联的
 {
-	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CCG10612Doc)));
-	return (CCG10612Doc*)m_pDocument;
+  ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CCG10612Doc)));
+  return (CCG10612Doc*)m_pDocument;
 }
-#endif //_DEBUG
-
+#endif  //_DEBUG
 
 // CCG10612View 消息处理程序
+
+double CCG10612View::MyStaticFunc_GetPointDistance(CPoint p1, CPoint p2) {
+#define sqr(x) ((double)(x) * (x))
+  double ans = sqrt(sqr(p1.x - p2.x) + sqr(p1.y - p2.y));
+  return ans;
+#undef sqr
+}
+
+void CCG10612View::MyStaticFunc_Warning(const char* n_File, int n_Line,
+                                        const char* n_Msg) {
+  fprintf(stderr, "Warning: %s\n - line %d: %s\n", n_File, n_Line, n_Msg);
+  fflush(stderr);
+}
+
+int CCG10612View::MyFunc_GetCursorOnNodeId(CPoint n_CursorPosition) {
+  for (auto& pr : m_NodeMap) {
+    const auto& nodePos = pr.second;
+    if (MyStaticFunc_GetPointDistance(nodePos.pos, n_CursorPosition) <=
+        NODE_RADIUS) {
+      return pr.first;
+    }
+  }
+  assert(m_NodeMap.count(UNDEFINED) == 0);
+  return UNDEFINED;
+}
+
+bool CCG10612View::MyFunc_CheckCursorOnNode(CPoint n_CursorPosition) {
+  return MyFunc_GetCursorOnNodeId(n_CursorPosition) != UNDEFINED;
+}
+
+void CCG10612View::MyFunc_TestAndDeleteNode(int nodeId) {
+  if (m_NodeMap.count(nodeId) > 0) {
+    auto nodePoint = m_NodeMap[nodeId];
+    if (nodePoint.pos.x <= 0 &&
+        nodePoint.pos.y <= 0) { /* 将结点移动到左上角之外就会被删除 */
+
+      m_NodeMap.erase(nodeId);
+    }
+  }
+}
+
+void CCG10612View::MyFunc_AddNewNodeAt(CPoint point) {
+  fprintf(stderr, "[CCG10612View::MyFunc_AddNewNodeAt] point = {%d,%d}\n",
+          point.x, point.y);
+  int nodeId = ++m_MaxNodeId;
+  CMyNode tmp;
+  tmp.nodeId = nodeId;
+  tmp.pos = point;
+  tmp.tag = UNDEFINED; /* undefined. */
+  m_NodeMap[nodeId] = tmp;
+}
+
+void CCG10612View::MyFunc_PutDownMovingNode() {
+  if (m_NodeMap.count(m_PickUpNodeId) > 0) {
+    MyFunc_TestAndDeleteNode(m_PickUpNodeId);
+    m_PickUpNodeId = UNDEFINED;
+  }
+}
+
+void CCG10612View::MyFunc_ShowAllItem() {
+  CDC* pDC = this->GetDC();
+  CRect zcRect;
+  this->GetClientRect(&zcRect);
+
+  /* 内存双缓冲 */
+  CDC dc;
+  dc.CreateCompatibleDC(pDC);
+  CBitmap memBmp;
+  memBmp.CreateCompatibleBitmap(pDC, zcRect.Width(),
+                                zcRect.Height()); /* 这里不能用 &dc */
+  CBitmap* OldBmp = dc.SelectObject(&memBmp);
+
+  /* 使用背景颜色清空屏幕 */
+  CRect rect;
+  GetClientRect(rect);
+  CBrush BkBrush;
+  BkBrush.CreateSolidBrush(BACKGROUND_COLOR);
+  dc.FillRect(rect, &BkBrush);
+
+  // TODO: 显示所有对象
+
+  MyFunc_ShowAllNode(&dc); /* 绘制所有结点 */
+
+  /* 最后要记得显示到屏幕上 */
+  pDC->BitBlt(rect.left, rect.top, zcRect.Width(), zcRect.Height(), &dc, 0, 0,
+              SRCCOPY);
+  ReleaseDC(pDC); /* 记得 release */
+}
+
+void CCG10612View::MyFunc_ShowAllNode(CDC* pDC) {
+  /* 绘制普通节点和选中结点时的画笔 */
+  BOOL ret;
+  CPen nodePen(PS_SOLID, NODE_WIDTH, NODE_COLOR);
+  CPen selectedPen(PS_SOLID, NODE_WIDTH, NODE_SELECTED_COLOR);
+
+  /* 绘制所有的结点 */
+  for (auto ptr = m_NodeMap.rbegin(); ptr != m_NodeMap.rend(); ptr++) {
+    const auto& nodePoint = ptr->second;
+
+    /* 获取矩形位置 */
+    RECT rect;
+    nodePoint.GetRect(&rect);
+
+    /* 选择合适的画笔 */
+    CPen* oldPen =
+        pDC->SelectObject(nodePoint.tag == UNDEFINED ? &nodePen : &selectedPen);
+
+    pDC->Ellipse(&rect); /* 绘制椭圆结点 */
+    pDC->SelectObject(oldPen);
+  }
+
+  /* 绘制所有的文字 */
+  for (auto ptr = m_NodeMap.rbegin(); ptr != m_NodeMap.rend(); ptr++) {
+    const auto& nodePoint = ptr->second;
+
+    RECT rect;
+    nodePoint.GetRect(&rect);
+
+    if (nodePoint.tag != UNDEFINED) {
+      wchar_t wbuf[30] = {};
+      wsprintf(wbuf, L"%d", nodePoint.tag);
+      RECT nRect = rect;
+      nRect.left += 2 * NODE_RADIUS;
+      nRect.top -= 2 * NODE_RADIUS;
+      nRect.right += 10 * NODE_RADIUS;
+      nRect.bottom += NODE_RADIUS;
+      pDC->DrawText(wbuf, &nRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+    }
+  }
+
+  nodePen.DeleteObject();
+  selectedPen.DeleteObject();
+}
+
+void CCG10612View::MyFunc_ChangeStateTo(int STATE_TO) {
+  m_State = STATE_TO;
+  for (auto& pr : m_NodeMap) {
+    auto& node = pr.second;
+    node.tag = UNDEFINED; /* < 0, undefined. */
+  }
+}
+
+void CCG10612View::OnLButtonDown(UINT nFlags, CPoint point) {
+  SetCapture(); /* 跟踪鼠标位置 */
+  m_LButtonDown = true;
+
+  switch (m_State) {
+    case STATE_FREE: /* 自由状态，点击鼠标左键按下 */
+      if (MyFunc_CheckCursorOnNode(point)) { /* 如果按到了结点 */
+        m_PickUpNodeId = MyFunc_GetCursorOnNodeId(point);
+        MyFunc_ChangeStateTo(STATE_MOVENODE); /* 切换到状态 MOVE */
+      }
+    case STATE_SETNODE:
+      /* 左键按下时不要响应，左键抬起时进行响应 */
+      break;
+    case STATE_MOVENODE:
+      MyWarning("[m_State: STATE_MOVENODE] LButtonDown.");
+      break;
+    case STATE_DELETENODE:
+      /* 左键按下时不响应，左键抬起时响应 */
+      break;
+      // TODO: 填充其他的自动机切换
+  }
+  CView::OnLButtonDown(nFlags, point);
+}
+
+void CCG10612View::OnLButtonUp(UINT nFlags, CPoint point) {
+  ReleaseCapture();
+  m_LButtonDown = false;
+
+  switch (m_State) {
+    case STATE_FREE:
+      break;
+    case STATE_SETNODE: /* 每次放下一个点就重新恢复自由状态 */
+      MyFunc_AddNewNodeAt(point);
+      MyFunc_ShowAllItem();
+      MyFunc_ChangeStateTo(STATE_FREE);
+      break;
+    case STATE_MOVENODE: /* 放下结点 */
+      MyFunc_PutDownMovingNode();
+      MyFunc_ChangeStateTo(STATE_FREE);
+      break;
+    case STATE_DELETENODE:
+      if (MyFunc_CheckCursorOnNode(point)) {
+        int nodeId = MyFunc_GetCursorOnNodeId(point);
+        m_NodeMap.erase(nodeId);
+      }
+      MyFunc_ChangeStateTo(STATE_FREE);
+      MyFunc_ShowAllItem();
+      break;
+      // TODO: 鼠标抬起事件
+  }
+  CView::OnLButtonUp(nFlags, point);
+}
+
+void CCG10612View::OnMouseMove(UINT nFlags, CPoint point) {
+  // TODO: 在此添加消息处理程序代码和/或调用默认值
+  CPoint oldCursorPos = m_CursorPos;
+  m_CursorPos = point;
+
+  switch (m_State) {
+    case STATE_FREE:
+      break;
+    case STATE_SETNODE:
+      break;
+    case STATE_MOVENODE:
+      m_NodeMap[m_PickUpNodeId].pos = point; /* 修改结点坐标 */
+      MyFunc_ShowAllItem();
+      break;
+    case STATE_DELETENODE:
+      break;
+      // TODO: MouseMove 事件处理
+  }
+  CView::OnMouseMove(nFlags, point);
+}
+
+void CCG10612View::OnSetnode() {
+  /* 点击了工具栏上的 SetNode 按钮 */
+
+  MyFunc_ChangeStateTo(STATE_SETNODE);
+}
+
+void CCG10612View::OnDeletenode() { MyFunc_ChangeStateTo(STATE_DELETENODE); }
+
+void CMyNode::GetRect(RECT* rect) const {
+  rect->left = pos.x - NODE_RADIUS;
+  rect->right = pos.x + NODE_RADIUS;
+  rect->top = pos.y - NODE_RADIUS;
+  rect->bottom = pos.y + NODE_RADIUS;
+}
--- CG10612/CG10612/CG10612View.h
+++ CG10612/CG10612/CG10612View.h
@@ -4,46 +4,100 @@
 
 #pragma once
 
+#include <map>
 
-class CCG10612View : public CView
-{
-protected: // 仅从序列化创建
-	CCG10612View() noexcept;
-	DECLARE_DYNCREATE(CCG10612View)
+struct CMyNode {
+  CPoint pos;                /* 结点位置 */
+  int nodeId;                /* 结点编号 */
+  int tag;                   /* 结点选中号 */
+  void GetRect(RECT*) const; /* 获得矩形范围 */
+};
+typedef std::map<int, CMyNode> CPointMap; /* 点坐标序列 */
 
-// 特性
-public:
-	CCG10612Doc* GetDocument() const;
+/* ---------- 我的常量 ---------- */
+#define NODE_RADIUS (5)                    /* 结点半径*/
+#define NODE_WIDTH (1)                     /* 结点线宽 */
+#define NODE_COLOR RGB(0, 0, 0)            /* 结点默认颜色 */
+#define NODE_SELECTED_COLOR RGB(255, 0, 0) /* 结点选中时颜色 */
 
-// 操作
-public:
+#define STATE_FREE (0)       /* 自由光标 */
+#define STATE_SETNODE (1)    /* 设置结点 */
+#define STATE_MOVENODE (2)   /* 移动节点 */
+#define STATE_DELETENODE (3) /* 移动节点 */
 
-// 重写
-public:
-	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
-	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
-protected:
-	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
-	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
-	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+#define BACKGROUND_COLOR RGB(255, 255, 255) /* 设置背景颜色 */
+#define UNDEFINED (-1)
 
-// 实现
-public:
-	virtual ~CCG10612View();
+class CCG10612View : public CView {
+ protected:  // 仅从序列化创建
+  CCG10612View() noexcept;
+  DECLARE_DYNCREATE(CCG10612View)
+
+  // 特性
+ public:
+  CCG10612Doc* GetDocument() const;
+
+  // 操作
+ public:
+  // 重写
+ public:
+  virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+  virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+ protected:
+  virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+  virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+  virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+  // 实现
+ public:
+  virtual ~CCG10612View();
 #ifdef _DEBUG
-	virtual void AssertValid() const;
-	virtual void Dump(CDumpContext& dc) const;
+  virtual void AssertValid() const;
+  virtual void Dump(CDumpContext& dc) const;
 #endif
 
-protected:
+ protected:
+  /* ---------- 我的变量 ---------- */
 
-// 生成的消息映射函数
-protected:
-	DECLARE_MESSAGE_MAP()
+  int m_MaxNodeId = 0; /* 最大结点编号 */
+  int m_State;         /* 自动机状态 */
+  int m_PickUpNodeId;  /* 当前正在被移动的 NodeId */
+  bool m_LButtonDown;  /* 鼠标左键是否按下 */
+  CPoint m_CursorPos;  /* 鼠标位置 */
+  CPointMap m_NodeMap; /* 结点列表 */
+
+  /* ---------- 我的函数 ---------- */
+
+  static double MyStaticFunc_GetPointDistance(CPoint, CPoint);
+  static void MyStaticFunc_Warning(const char* n_File, int n_Line,
+                                   const char* n_Msg);
+
+/* 用于报错的宏 */
+#define MyWarning(n_Msg) MyStaticFunc_Warning(__FILE__, __LINE__, n_Msg)
+
+  int MyFunc_GetCursorOnNodeId(CPoint n_CursorPosition);
+  bool MyFunc_CheckCursorOnNode(CPoint n_CursorPosition);
+  void MyFunc_TestAndDeleteNode(int nodeId); /* 检测结点是否在左上角之外 */
+  void MyFunc_AddNewNodeAt(CPoint point);  /* 防止新节点 */
+  void MyFunc_PutDownMovingNode();         /* 放下正在移动的节点 */
+  void MyFunc_ShowAllItem();               /* 显示所有对象 */
+  void MyFunc_ShowAllNode(CDC* pDC);       /* 显示所有结点 */
+  void MyFunc_ChangeStateTo(int STATE_TO); /* 设置新状态 */
+
+  // 生成的消息映射函数
+ protected:
+  DECLARE_MESSAGE_MAP()
+ public:
+  afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+  afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+  afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+  afx_msg void OnSetnode();
+  afx_msg void OnDeletenode();
 };
 
 #ifndef _DEBUG  // CG10612View.cpp 中的调试版本
-inline CCG10612Doc* CCG10612View::GetDocument() const
-   { return reinterpret_cast<CCG10612Doc*>(m_pDocument); }
+inline CCG10612Doc* CCG10612View::GetDocument() const {
+  return reinterpret_cast<CCG10612Doc*>(m_pDocument);
+}
 #endif
-
